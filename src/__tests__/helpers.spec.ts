a8e46a76a3c7138346fae7ff90de6b4b3aaaf1b8
Rassell <sordesos@gmail.com>
2019-07-11 01:05:32 +0200

    feat: test and snapshots

--- src/__tests__/helpers.spec.ts
+++ src/__tests__/helpers.spec.ts
@@ -3,23 +3,19 @@ import { flatten, makePoint, makeLine } from '../helpers';
 test('makePoint should return point object', () => {
   const point = makePoint([0, 1]);
 
-  expect(point).toEqual(
-    expect.objectContaining({
-      latitude: expect.any(Number),
-      longitude: expect.any(Number)
-    })
-  );
+  expect(point).toMatchSnapshot({
+    latitude: expect.any(Number),
+    longitude: expect.any(Number)
+  });
 });
 
 test('makeLine should return array point object', () => {
   const line = makeLine([[0, 1], [0, 1]]);
 
   line.forEach((point: any) => {
-    expect(point).toEqual(
-      expect.objectContaining({
-        latitude: expect.any(Number),
-        longitude: expect.any(Number)
-      })
-    );
+    expect(point).toMatchSnapshot({
+      latitude: expect.any(Number),
+      longitude: expect.any(Number)
+    });
   });
 });
