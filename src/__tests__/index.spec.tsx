9049f0357095db2d84758b02f5eb0d793ee6f0bf
Rassell <sordesos@gmail.com>
2019-07-10 20:48:14 +0200

    WIP

---
+++ src/__tests__/index.spec.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Text } from 'react-native';
+import MapView from 'react-native-maps';
+import TestRenderer from 'react-test-renderer';
+import { FeatureCollection } from 'geojson';
+
+import Geojson from '..';
+
+test('makePoint should return point object', () => {
+  const alcatraz: FeatureCollection = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'Point',
+          coordinates: [-122.42305755615234, 37.82687023785448]
+        }
+      }
+    ]
+  };
+
+  const mapView = TestRenderer.create(<Geojson geojson={alcatraz} />).toJSON();
+
+  expect(mapView).toMatchSnapshot();
+});
